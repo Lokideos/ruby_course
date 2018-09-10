56850ad3dda4e5d5c8ffb918be17416f3e217a46
lokideos <archmage09@gmail.com>
2018-09-11 02:53:00 +0300

    Quadratic equation

---
+++ homeworks/lesson1_ruby_basics/quadratic_equation.rb
@@ -0,0 +1,16 @@
+class QuadraticEquation
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def calculate_discriminant
+    @D = @b**2 - 4*@a*@c
+  end
+
+  def calculate_roots
+    return [(-@b + Math.sqrt(@D))/2/@a, (-@b - Math.sqrt(@D))/2/@a] if @D > 0
+    -@b/2*@a if @D == 0
+  end
+end
