c97792c9804319a645532dc591f5c7b3ac501d36
lokideos <archmage09@gmail.com>
2018-09-13 17:37:55 +0300

    Basket with goods

---
+++ homeworks/lesson2_ruby_basics_p2/basket_with_goods.rb
@@ -0,0 +1,38 @@
+class BasketWithGoods
+
+  def initialize(tax=0)
+    @basket = {}
+    @total = 0
+    @tax = tax
+  end
+
+  def fill_with_goods
+    loop do
+      puts "Type in product name:"
+      product = gets.chomp
+      break if product.downcase == "stop"
+      puts "Type in price for #{product}:"
+      price = gets.chomp.to_i
+      puts "Type in quantity of #{product}:"
+      quantity = gets.chomp.to_i
+      product_total = price * quantity
+      @basket[product] = { price: price, quantity: quantity, product_total: product_total }
+      @total += product_total
+    end
+  end
+
+  def show_goods
+    @basket.each do |product, price_information|
+      puts %Q(
+      ____________________________________________________________________
+      #{product}: 
+      price for each item: #{price_information[:price]},
+      quantity of #{product}: #{price_information[:quantity]},
+      total price for #{product}: #{price_information[:product_total]}
+      ____________________________________________________________________
+      )
+    end
+
+    puts "Total price for your goods: #{@total}"
+  end
+end
