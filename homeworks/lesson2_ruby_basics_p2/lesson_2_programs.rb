74c8bbd425941372be4fb589947147f093ab0962
lokideos <archmage09@gmail.com>
2018-09-13 16:26:25 +0300

    Fill hash with vowels

--- homeworks/lesson2_ruby_basics_p2/lesson_2_programs.rb
+++ homeworks/lesson2_ruby_basics_p2/lesson_2_programs.rb
@@ -1,3 +1,4 @@
 require_relative '30_days_months'
 require_relative 'fill_array_with_designated_range'
 require_relative 'fill_array_with_fibonacci_numbers'
+require_relative 'fill_hash_with_vowels'
