a9f5eabacccb01524b02c62f3375b03b24daac45
lokideos <archmage09@gmail.com>
2018-09-14 00:59:25 +0300

    Railway Route

---
+++ railroad/railroad_actions/route.rb
@@ -0,0 +1,15 @@
+class Route
+  attr_reader :name, :stations
+
+  def initialize(first_station, last_station)
+    @stations=[first_station, last_station]
+  end
+
+  def add_station(station)
+    @stations.insert(2, station)
+  end
+
+  def remove_station(station)
+    @stations.delete(station) unless station == @station[0] || station == @station[-1]
+  end
+end
