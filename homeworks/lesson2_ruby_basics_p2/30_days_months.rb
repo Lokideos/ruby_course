b6137e9fd5ebcb268f697ba298336bb88291a918
lokideos <archmage09@gmail.com>
2018-09-13 15:09:02 +0300

    Find months with 30 days

---
+++ homeworks/lesson2_ruby_basics_p2/30_days_months.rb
@@ -0,0 +1,11 @@
+class ThirtyDaysMonths
+  MONTHS = {
+    january: 31, february: 28, march: 31, april: 30,
+    may: 31, june: 30, july: 31, august: 31,
+    september: 30, october: 31, november: 30, december: 31
+  }
+  
+  def self.print_30_days_months
+    MONTHS.each { |month, days| puts month.to_s if days == 30 }
+  end
+end
