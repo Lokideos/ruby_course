08e2a5ecd8c28b3b84212720147680d808bd00bb
lokideos <archmage09@gmail.com>
2018-09-14 00:52:40 +0300

    Railway Station

---
+++ railroad/railroad_actions/station.rb
@@ -0,0 +1,22 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize (name)
+    @name = name
+    @trains = []
+  end
+
+  def arrival_of_train(train)
+    @trains.push(train)
+  end
+
+  def trains_of_type(train_type)
+    trains_of_type = []
+    @trains.each { |train| trains_of_type.push(train) if train.type == train_type }
+    trains_of_type
+  end
+
+  def departure_of_train(train)
+    @trains.delete(train)
+  end
+end
