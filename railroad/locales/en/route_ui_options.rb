fdd0f23cd8bf4e4f63a5c45ef55ddf106c4f669a
lokideos <archmage09@gmail.com>
2018-09-14 16:34:47 +0300

    Refatoring classes with too many class methods

--- railroad/locales/en/route_ui_options.rb
+++ railroad/locales/en/route_ui_options.rb
@@ -1,52 +1,54 @@
 class RouteUIOptions
-  def self.show_existing_stations_ad
-    puts "List of existing stations:"
-  end
+  class << self
+    def show_existing_stations_ad
+      puts "List of existing stations:"
+    end
 
-  def self.create_route_prompt
-    puts %q(
-    Please type in names of first station of the route and last station of the route 
-    in the respective order.
-    )
-  end
+    def create_route_prompt
+      puts %q(
+      Please type in names of first station of the route and last station of the route 
+      in the respective order.
+      )
+    end
 
-  def self.route_created_ad
-    puts "Route has been succesfully created."
-  end
+    def route_created_ad
+      puts "Route has been succesfully created."
+    end
 
-  def self.wrong_station_names_ad
-    puts "You typed in wrong station names. Please try again."
-  end
+    def wrong_station_names_ad
+      puts "You typed in wrong station names. Please try again."
+    end
 
-  def self.route_name_prompt
-    puts "Please type in desired route name."
-  end
+    def route_name_prompt
+      puts "Please type in desired route name."
+    end
 
-  def self.route_does_not_exist
-    puts "Route with this name does not exist."
-  end
+    def route_does_not_exist
+      puts "Route with this name does not exist."
+    end
 
-  def self.station_add_to_route_ad
-    puts "Station has been succesfully added to route."
-  end
+    def station_add_to_route_ad
+      puts "Station has been succesfully added to route."
+    end
 
-  def self.show_route_stations_ad
-    puts "List of station existing on the route:"
-  end
+    def show_route_stations_ad
+      puts "List of station existing on the route:"
+    end
 
-  def self.choose_station_to_delete_prompt
-    puts "Please type in name of station you want to delete:"
-  end
+    def choose_station_to_delete_prompt
+      puts "Please type in name of station you want to delete:"
+    end
 
-  def self.station_deleted_ad
-    puts "Station has been succesfully deleted from the route."
-  end
+    def station_deleted_ad
+      puts "Station has been succesfully deleted from the route."
+    end
 
-  def self.show_existing_routes_ad
-    puts "List of existing routes:"
-  end
+    def show_existing_routes_ad
+      puts "List of existing routes:"
+    end
 
-  def self.add_station_to_route_prompt
-    puts "Please type in name of the station you want to add to the route."
+    def add_station_to_route_prompt
+      puts "Please type in name of the station you want to add to the route."
+    end
   end
 end
--- railroad/locales/en/train_ui_options.rb
+++ railroad/locales/en/train_ui_options.rb
@@ -1,78 +1,80 @@
 class TrainUIOptions
-  def self.train_creation_prompt
-    puts %q(
-    In order to create new train
-    you have to type in train's number, 
-    type, which have to be 'freight' or 'passenger',
-    and cars' quantity in the respective order:
-    )
-  end
+  class << self
+    def train_creation_prompt
+      puts %q(
+      In order to create new train
+      you have to type in train's number, 
+      type, which have to be 'freight' or 'passenger',
+      and cars' quantity in the respective order:
+      )
+    end
 
-  def self.show_existing_trains_ad
-    puts "List of trains created so far:"
-  end
+    def show_existing_trains_ad
+      puts "List of trains created so far:"
+    end
 
-  def self.train_number_prompt
-    puts "Please type in number of a train."
-  end
+    def train_number_prompt
+      puts "Please type in number of a train."
+    end
 
-  def self.train_does_not_exist
-    puts "Train with this number does not exist."
-  end
+    def train_does_not_exist
+      puts "Train with this number does not exist."
+    end
 
-  def self.speed_up_ad
-    puts "Train's speed has been successfully increased by 10."
-  end
+    def speed_up_ad
+      puts "Train's speed has been successfully increased by 10."
+    end
 
-  def self.speed_down_ad
-    puts "Train's speed has been successfully decreased by 10."
-  end
+    def speed_down_ad
+      puts "Train's speed has been successfully decreased by 10."
+    end
 
-  def self.current_speed_ad
-    print "Train's current speed:"
-  end
+    def current_speed_ad
+      print "Train's current speed:"
+    end
 
-  def self.show_cars_quantity_ad
-    puts "Train's current cars' quantity:"
-  end
+    def show_cars_quantity_ad
+      puts "Train's current cars' quantity:"
+    end
 
-  def self.car_atttached_ad
-    puts "Car has been successfully attached to this train."
-  end
+    def car_atttached_ad
+      puts "Car has been successfully attached to this train."
+    end
 
-  def self.car_detached_ad
-    puts "Car has been successfully detached from this train."
-  end
+    def car_detached_ad
+      puts "Car has been successfully detached from this train."
+    end
 
-  def self.high_speed_warning
-    puts "Speed of the train is too high for this operation. Try to change it to 0."
-  end
+    def high_speed_warning
+      puts "Speed of the train is too high for this operation. Try to change it to 0."
+    end
 
-  def self.choose_route_prompt
-    puts "Please type in name of route you want to assign to this train."
-  end
+    def choose_route_prompt
+      puts "Please type in name of route you want to assign to this train."
+    end
 
-  def self.show_routes_ad
-    puts "List of existing routes:"
-  end
+    def show_routes_ad
+      puts "List of existing routes:"
+    end
 
-  def self.route_assigned_ad
-    puts "Route has been successfully assigned to this train."
-  end
+    def route_assigned_ad
+      puts "Route has been successfully assigned to this train."
+    end
 
-  def self.route_doesn_not_exist
-    puts "Route with this name does not exists."
-  end
+    def route_doesn_not_exist
+      puts "Route with this name does not exists."
+    end
 
-  def self.route_not_assigned_ad
-    puts "Route has not been assigned to this train."
-  end
+    def route_not_assigned_ad
+      puts "Route has not been assigned to this train."
+    end
 
-  def self.nearby_stations_ad
-    puts "List of nearby stations on the route:"
-  end
+    def nearby_stations_ad
+      puts "List of nearby stations on the route:"
+    end
 
-  def self.choose_direction_on_route_prompt
-    puts "Please type in direction in which you want to move on the current route."
+    def choose_direction_on_route_prompt
+      puts "Please type in direction in which you want to move on the current route."
+    end
   end
 end
